1430714cdaef50a6c6c7298c5c69e45bb743ed2f
James Sangalli <j.l.sangalli@gmail.com>
2020-05-21 14:27:42 +1000

    add base code for GFO.sol

---
+++ contracts/GFO.sol
@@ -0,0 +1,100 @@
+/*
+Requirements:
+· total 100,000,000.00, 8 decimal,
+· white list: only people in white list can transfer or receive GFT
+· admin functions: move token, manage white list
+.*/
+
+
+pragma solidity >=0.4.22 <0.6.0;
+
+import "./EIP20Interface.sol";
+
+contract GFO is EIP20Interface {
+
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowed;
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show.
+    string public symbol;                 //An identifier: eg SBX
+    mapping (address => bool) public whitelisted;           //Addresses that are allowed to transfer or receive GFT
+    address[] public whitelistAddresses; //keep track of addresses in the whitelist
+    address public admin;
+
+    modifier isOnWhiteList(address user) {
+        require(whitelist[user] == true || user == admin);
+        _;
+    }
+
+    modifier adminOnly() {
+        require(msg.sender == admin);
+        _;
+    }
+
+    constructor(
+        uint256 _initialAmount,
+        string _tokenName,
+        uint8 _decimalUnits,
+        string _tokenSymbol,
+        address _admin,
+        address[] _approvedAddresses
+    ) public {
+        balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
+        totalSupply = _initialAmount;                        // Update total supply
+        name = _tokenName;                                   // Set the name for display purposes
+        decimals = _decimalUnits;                            // Amount of decimals for display purposes
+        symbol = _tokenSymbol;                               // Set the symbol for display purposes
+        admin = _admin;
+        whiteListAddresses = _approvedAddresses;
+    }
+
+    function addToWhiteList(address[] approvedAddresses) public adminOnly {
+        for(int i = 0; i < approvedAddresses.length; i++) {
+            whitelistAddresses.push(approvedAddresses[i]);
+            whitelisted[approvedAddresses[i]] = true;
+        }
+    }
+
+    function transfer(address _to, uint256 _value) public isOnWhiteList(msg.sender) returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public isOnWhiteList(msg.sender) returns (bool success) {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
+        }
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public isOnWhiteList(spender) returns (bool success) {
+        isOnWhiteList(msg.sender);
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+}
